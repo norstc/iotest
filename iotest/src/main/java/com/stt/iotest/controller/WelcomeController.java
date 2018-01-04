04f8d51eb2108a59ae234ef97362ee16bff7e684
norstc <norstc@gmail.com>
2018-01-04 15:19:37 +0800

    basic controller and freemarker done

---
+++ iotest/src/main/java/com/stt/iotest/controller/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.stt.iotest.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+	private static final Logger log = LoggerFactory.getLogger(WelcomeController.class);
+	
+	@RequestMapping("/")
+	public String homeHandler(){
+		return "welcome";
+	}
+}
